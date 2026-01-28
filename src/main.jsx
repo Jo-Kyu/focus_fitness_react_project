5abaa6cbc47e7f874878d3875cd37d9358f37eff
KOMATSUPEI <komatsupei@gmail.com>
2026-01-29 06:54:28 +0800

    feat:main.js新增CartStepThree引入，渲染調整回ProductDetail

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-// import ProductDetail from "./pages/ProductDetail";
+import ProductDetail from "./pages/ProductDetail";
 import CartStepThree from "./pages/ CartStepThree";
 import "./assets/scss/all.scss";
 import "bootstrap";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <ProductDetail /> */}
-    <CartStepThree />
+    <ProductDetail />
+    {/* <CartStepThree /> */}
   </StrictMode>,
 );
