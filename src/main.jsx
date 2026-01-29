f919d878a0446ae09d3e84bf857c4f1d4870ea04
KOMATSUPEI <komatsupei@gmail.com>
2026-01-30 03:39:45 +0800

    feat:Footer元件轉JSX

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import ProductDetail from "./pages/ProductDetail";
 import CartStepThree from "./pages/ CartStepThree";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import "./assets/scss/all.scss";
 import "bootstrap";
 
@@ -12,5 +13,6 @@ createRoot(document.getElementById("root")).render(
     <ProductDetail />
     {/* <CartStepThree /> */}
     {/* <Header /> */}
+    {/* <Footer /> */}
   </StrictMode>
 );
