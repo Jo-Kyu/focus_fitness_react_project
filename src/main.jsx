7a3fc04397b5a5b39824ee6951ad7f11cb9c7c1d
Jo <test@gmail.com>
2026-01-30 19:30:46 +0800

    feat:轉JSX、購物車區塊邏輯、輪播邏輯

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import ProductDetail from "./pages/ProductDetail";
+import App from "./App";
 import "./assets/scss/all.scss";
 import "bootstrap";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ProductDetail />
+    <App />
   </StrictMode>,
 );
