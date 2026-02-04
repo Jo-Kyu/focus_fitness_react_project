3ca94c993b9412eebf6f8c6ffdc3c5544c094ec6
Jo <test@gmail.com>
2026-02-04 11:12:17 +0800

    feat:新增提示框功能、新增吐司功能

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Toaster } from "react-hot-toast";
 
 import App from "./App";
 import "./assets/scss/all.scss";
 import "bootstrap";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    {/* 全域 Toast 掛載 */}
+    <Toaster position="top-right" />
     <App />
   </StrictMode>,
 );
