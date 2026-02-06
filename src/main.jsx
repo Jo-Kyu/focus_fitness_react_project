3c74362ea4d0707a91b46df51e009586c6d474a3
KOMATSUPEI <komatsupei@gmail.com>
2026-02-07 00:39:40 +0800

    fix:main更改回App渲染

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,12 @@ import { createRoot } from "react-dom/client";
 import { Toaster } from "react-hot-toast";
 
 import App from "./App";
-import Home from "./pages/Home";
 import "./assets/scss/all.scss";
 import "bootstrap";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Toaster possition="top-right" />
-    <Home />
+    <App />
   </StrictMode>,
 );
