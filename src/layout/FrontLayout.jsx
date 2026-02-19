ba6f84191a03157c3375ade3c6906bd8ac97114f
KOMATSUPEI <komatsupei@gmail.com>
2026-02-20 05:47:43 +0800

    fix:BackTop元件的404錯誤修正、新增頁面跳轉自動導向頁首

--- src/layout/FrontLayout.jsx
+++ src/layout/FrontLayout.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router";
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import ScrollToTop from "../components/ScrollToTop";
+
 
 function FrontLayout() {
   
 
   return (
     <>
+      <ScrollToTop /> 
       <Header />
           <Outlet />
       <Footer />
