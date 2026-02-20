761ab2bc9a56f8b5523d0de5636b446e57d63d54
KOMATSUPEI <komatsupei@gmail.com>
2026-02-20 20:34:02 +0800

    feat:增加商品列表卡片收藏功能

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,6 +1,15 @@
 // React Hooks
-import { useEffect, useState, useMemo, useRef, useCallback } from "react";
-import { NavLink, useLocation } from "react-router";
+import { useEffect, useState, useMemo, useRef, useCallback, useContext } from "react";
+import { NavLink, useLocation, useNavigate } from "react-router";
+
+// 收藏共用狀態
+import { WishlistContext } from "../context/WishlistContext.js";
+// 登入共用狀態
+import { LoginAuthContext } from "../context/LoginAuthContext.js";
+// 吐司
+import toast from "react-hot-toast";
+// 提示框
+import Swal from "sweetalert2";
 
 // 元件
 import BackTop from "../components/BackTop.jsx";
@@ -55,7 +64,7 @@ const CATEGORIES_CONFIG = {
   }
 };
 
-function ProductList(){
+function ProductList({product}){
     // 定義遠端取得的商品狀態
     const [allProducts, setAllProducts] = useState([]);
     // 定義商品列表狀態
@@ -79,7 +88,16 @@ function ProductList(){
 
      // 定義手機offcanvas
     const offcanvasRef = useRef(null); 
-    const offcanvasInstance = useRef(null); 
+    const offcanvasInstance = useRef(null);
+    
+    // 收藏共用狀態解構
+    const { wishlist, toggleWishlistItem } = useContext(WishlistContext);
+    // const isFavorite = wishlist[product.id];
+    // 登入共用狀態解構
+    const { isAuth } = useContext(LoginAuthContext);
+
+    // 導向至登入頁
+    const navigate = useNavigate();
 
 
     // ============ 取得所有遠端商品 ============
@@ -318,7 +336,7 @@ function ProductList(){
             {/* 手機版上方選單 */}
             <ul className="nav nav-pills d-md-none phone_nav">
                 <li className="nav-item me-6 nav-item_new">
-                    <a class="nav-link ruby-text active" 
+                    <a className="nav-link ruby-text active" 
                         onClick={(e) => {
                             e.preventDefault();
                             setSelectedMainCategory("all");
@@ -782,6 +800,7 @@ function ProductList(){
                                 {/* 商品 */}
                                 {productList.length>0 ?(
                                     productList.map((product)=>{
+                                        const isFavorite = wishlist[product.id];
                                         return (
                                             <div className="col-6 col-sm-4 plr-10 mb-6 mb-md-7" key={product.id}>
                                                 <div className="card rounded-3 bg-blue-600 flex-grow-0 flex-shrink-0 hover-effect-3 overflow-hidden">
@@ -805,22 +824,89 @@ function ProductList(){
                                                                     </span>
                                                                 </div>
                                                                 {/* 收藏符號 */}
-                                                                <div className="p-2 color-primary-400 bg-gray-900-20 blur-30 rounded-2 border-top-left-radius-0 border-bottom-right-radius-0 ms-auto">
-                                                                    <svg
-                                                                        width="24"
-                                                                        height="24"
-                                                                        viewBox="0 0 36 37"
-                                                                        fill="none"
+                                                                <button
+                                                                type="button"
+                                                                className="p-2 color-primary-400 bg-gray-900-20 blur-30 rounded-2 border-top-left-radius-0 border-bottom-right-radius-0 ms-auto z-100 border-0"
+                                                                onClick={(e) => {
+                                                                e.preventDefault();
+                                                                e.stopPropagation();
+                                                                // 判斷登入狀態
+                                                                if (!isAuth) {
+                                                                    Swal.fire({
+                                                                    title: "您尚未登入帳號",
+                                                                    text: "登入帳號後，才可使用收藏功能！",
+                                                                    iconHtml: `<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="#e1ff00" className="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
+                                                                        <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+                                                                        </svg>`,
+                                                                    showCancelButton: true, // 顯示取消按鈕
+                                                                    reverseButtons: true, // 按鈕位置對調
+                                                                    confirmButtonText: "前往登入！",
+                                                                    cancelButtonText: "取消！",
+
+                                                                    customClass: {
+                                                                        popup: "handleAddToCartToast",
+                                                                        confirmButton: "cancelButton",
+                                                                        cancelButton: "confirmButton",
+                                                                    },
+                                                                    }).then((result) => {
+                                                                        if (result.isConfirmed) {
+                                                                            navigate("/login"); // 確認後導向登入頁
+                                                                        }
+                                                                        });
+                                                                    // console.log(isAuth);
+                                                                    // console.log("未登入");
+                                                                    return;
+                                                                }
+                                                                toggleWishlistItem(product.id);
+                                                                if (!isFavorite) {
+                                                                    toast.success("已加入收藏", {
+                                                                    className: "handleAddToCartToast",
+                                                                    icon: (
+                                                                        <svg
                                                                         xmlns="http://www.w3.org/2000/svg"
-                                                                    >
-                                                                        <path
-                                                                        d="M9.28592 33.2821C8.54171 33.8176 7.50391 33.2856 7.50391 32.3688V9.875C7.50391 7.18261 9.68651 5 12.3789 5H23.6265C26.3188 5 28.5015 7.18261 28.5015 9.875V32.3688C28.5015 33.2856 27.4636 33.8176 26.7195 33.2821L18.0027 27.0107L9.28592 33.2821ZM26.2515 9.875C26.2515 8.42525 25.0762 7.25 23.6265 7.25H12.3789C10.9292 7.25 9.75391 8.42525 9.75391 9.875V30.1736L17.3457 24.7115C17.7382 24.4292 18.2673 24.4292 18.6597 24.7115L26.2515 30.1736V9.875Z"
+                                                                        width="36"
+                                                                        height="37"
                                                                         fill="#e1ff00"
                                                                         stroke="#e1ff00"
-                                                                        strokeWidth="0.5"
-                                                                        />
-                                                                    </svg>
-                                                                </div>
+                                                                        viewBox="0 0 16 20"
+                                                                        >
+                                                                        <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2" />
+                                                                        </svg>
+                                                                    ),
+                                                                    });
+                                                                } else {
+                                                                    toast.error("已取消收藏", {
+                                                                    className: "handleAddToCartToast",
+                                                                    icon: (
+                                                                        <svg
+                                                                        xmlns="http://www.w3.org/2000/svg"
+                                                                        width="36"
+                                                                        height="36"
+                                                                        fill="#ff514f"
+                                                                        className="bi bi-x-square-fill"
+                                                                        viewBox="0 0 16 16"
+                                                                        >
+                                                                        <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708" />
+                                                                        </svg>
+                                                                    ),
+                                                                    });
+                                                                }
+                                                                }}
+                                                            >
+                                                                <svg
+                                                                xmlns="http://www.w3.org/2000/svg"
+                                                                width="30"
+                                                                height="31"
+                                                                fill={isFavorite ? "#e1ff00" : "none"}
+                                                                stroke="#e1ff00"
+                                                                className="bi bi-bookmark-fill"
+                                                                viewBox="0 0 16 20"
+                                                                >
+                                                                <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2" />
+                                                                </svg>
+                                                                </button>
+
+
                                                             </div>
                                                         </div>
                                                         {/* 商品標題、價格、折扣標籤 */}
