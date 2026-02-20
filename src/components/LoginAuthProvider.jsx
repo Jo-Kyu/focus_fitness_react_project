1300c7895fcb146610991542b3a88f0a7de8492e
Jo <test@gmail.com>
2026-02-20 17:06:47 +0800

    fix:解決衝突

--- src/components/LoginAuthProvider.jsx
+++ src/components/LoginAuthProvider.jsx
@@ -42,14 +42,14 @@ export function LoginAuthProvider({ children }) {
         Swal.fire({
           title: "登入成功 !",
           iconHtml: ` <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="100"
-                                height="100"
-                                fill="#e1ff00"
-                               className="bi bi-check-square-fill"
-                                viewBox="0 0 16 16"
-                              >
-                                <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z" />
+                    xmlns="http://www.w3.org/2000/svg"
+                      width="100"
+                      height="100"
+                      fill="#e1ff00"
+                      className="bi bi-check-square-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z" />
                     </svg>`,
           confirmButtonText: "立即購物！",
           customClass: {
