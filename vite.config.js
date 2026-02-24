3168b86ef57dbde7f6e47a278b2b88e3b82c9b5b
Jo <test@gmail.com>
2026-02-24 22:48:37 +0800

    部屬前置作業

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base:
+    process.env.NODE_ENV === "production"
+      ? "/focus_fitness_react_project/" // 部署 GitHub Pages
+      : "/", // 本地開發
+});
